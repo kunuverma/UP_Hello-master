6d480aa43df316172546b9721e793ab1615d18de
kunalverma <vermakunal15092000@gmail.com>
2023-08-02 10:02:33 +0530

    Hello

--- pages/assets/js/navbar.js
+++ pages/assets/js/navbar.js
@@ -41,6 +41,62 @@ window.addEventListener("resize", function () {
   }
 });
 
+function allCategory() {
+  let icon = document.getElementById("drop-down-icon");
+
+  let page = document.getElementById("drop-down-page");
+  if (page.classList.contains("hidden")) {
+    page.classList.remove("hidden");
+    page.classList.add("flex");
+    setTimeout(() => {
+      page.classList.remove("opacity-in");
+    }, 500);
+    icon.style.transform = "rotate(180deg)";
+  } else {
+    page.classList.add("opacity-out");
+    setTimeout(() => {
+      page.classList.remove("flex");
+      page.classList.add("hidden");
+      page.classList.remove("opacity-out");
+      page.classList.add("opacity-in");
+    }, 400);
+    icon.style.transform = "rotate(0deg)";
+  }
+}
+
+function profileBoxToggle() {
+  let box = document.getElementById("profile-box");
+  if (box.classList.contains("hidden")) {
+    box.classList.remove("hidden");
+    box.classList.add("flex");
+  } else {
+    box.classList.remove("flex");
+    box.classList.add("hidden");
+  }
+}
+
+// Hiding profile box when screen is smaller
+window.addEventListener("resize", function () {
+  if (window.innerWidth < 1130) {
+    document.getElementById("profile-box").classList.remove("flex");
+    document.getElementById("profile-box").classList.add("hidden");
+  }
+});
+
+//Toggling location box's display property
+function locationBoxToggle() {
+  const arrow = document.getElementById("location-arrow");
+
+  const box = document.getElementById("location-box");
+  if (box.classList.contains("hidden")) {
+    box.classList.remove("hidden");
+    arrow.style.transform = "rotate(180deg)";
+  } else {
+    box.classList.add("hidden");
+    arrow.style.transform = "rotate(0deg)";
+  }
+}
+
 function onFavClick(e) {
   e.preventDefault();
   alert("worked!");
